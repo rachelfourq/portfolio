5308122ddd8b79369b19cb843f0878d894021dca
rachelfourq <rachel.lake@rocketmail.com>
2016-01-07 11:09:29 -0800

    smooth scroll working

--- script.js
+++ script.js
@@ -6,40 +6,5 @@ $(document).ready(function() {
     margin: 10
   });
 
-// not working
-$('#fullpage').fullpage({
-  lockAnchors: false,
-  anchors:['first', 'second', 'third'],
-
-
-  // Scrolling
-  css3: true,
-  scrollingSpeed: 700,
-  autoScrolling: true,
-  fitToSection: true,
-  fitToSectionDelay: 1000,
-  scrollBar: false,
-  easing: 'easeInOutCubic',
-  easingcss3: 'ease',
-
-
-  // Accessibility
-  keyboardScrolling: true,
-  animateAnchor: true,
-  recordHistory: true,
-
-
-
-  // Custom Selectors
-  sectionSelector: '.section',
-
-
-  // Events
-  onLeave: function(index, nextIndex, direction){},
-  afterLoad: function(anchorLink, index){},
-  afterRender: function(){},
-  afterResize: function(){},
-  afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
-  onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
-});
+  $('a').smoothScroll();
 });
